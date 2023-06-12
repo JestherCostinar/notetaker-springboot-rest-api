c4ce377b8bdcb18872a4adbd736c186a2b46bc23
Jesther Costinar <jesther.jc15@gmail.com>
2023-06-12 18:29:12 +0800

    Note entity/models

---
+++ src/main/java/com/jesthercostinar/notetakingspringbootrestapi/entity/Note.java
@@ -0,0 +1,39 @@
+package com.jesthercostinar.notetakingspringbootrestapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_note")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String body;
+
+    @Column(nullable = false)
+    private boolean hidden;
+
+    @CreationTimestamp
+    private LocalDateTime dateCreate;
+
+    @UpdateTimestamp
+    private LocalDateTime lastDateUpdate;
+}
