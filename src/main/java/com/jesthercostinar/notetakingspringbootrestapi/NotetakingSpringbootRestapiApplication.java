e77ae413b71ccc118f0d87418b3d8cd8296c4f97
Jesther Costinar <jesther.jc15@gmail.com>
2023-06-12 18:19:55 +0800

    first commit

---
+++ src/main/java/com/jesthercostinar/notetakingspringbootrestapi/NotetakingSpringbootRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.jesthercostinar.notetakingspringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotetakingSpringbootRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotetakingSpringbootRestapiApplication.class, args);
+	}
+
+}
